79421dd20a59812cfbe8a79dcdb3cee10ba267da
Akansha <akanshabharti1213@gmail.com>
2024-06-21 23:13:49 +0530

    responsive navbar

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
+  return <Wrapper className="test"></Wrapper>;
 };
 
 const Wrapper = styled.section`
+  height: 100vh;
   background-color: ${({ theme }) => theme.colors.bg};
  
 `;
