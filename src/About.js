5d2a36c0ca6d876d24398d3549f6f7366bbcd0f7
Akansha <akanshabharti1213@gmail.com>
2024-06-21 23:44:42 +0530

    Home page section one done

--- src/About.js
+++ src/About.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const About = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const data = {
+    name: "24x7 Ecommerce"
+  }
+  return <HeroSection myData={data}/>;
 }
 
 export default About
--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
-import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
 
 const Home = () => {
-  return <Wrapper className="test"></Wrapper>;
+  const data = {
+    name: "24x7 Store"
+  }
+  return <HeroSection myData={data}/>;
 };
 
-const Wrapper = styled.section`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.bg};
- 
-`;
+
 
 export default Home; 
