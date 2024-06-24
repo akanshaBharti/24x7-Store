2259e5695b8e88801d070bea07536cc720661d2f
Akansha <akanshabharti1213@gmail.com>
2024-06-24 23:41:39 +0530

    section 2 home page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import HeroSection from './components/HeroSection'
+import { AppContext, useProductContext } from './context/productContext'
 
 const About = () => {
+  const {myName} = useProductContext();
+
   const data = {
     name: "24x7 Ecommerce"
   }
-  return <HeroSection myData={data}/>;
+  return (
+    <>
+    {myName}
+     <HeroSection myData={data}/>;
+    </>
+  )
+
+ 
 }
 
 export default About
