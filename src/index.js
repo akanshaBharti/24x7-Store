678d24100e130bc942ef2abcb56705aa6624969f
Akansha <akanshabharti1213@gmail.com>
2024-07-02 02:33:05 +0530

    cartcontext

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AppProvider } from './context/productContext';
-import { FilterContextProvider } from './context/filterContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AppProvider } from "./context/productContext";
+import { FilterContextProvider } from "./context/filterContext";
+import { CartProvider } from "./context/cartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AppProvider>
     <FilterContextProvider>
-    <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </FilterContextProvider>
-  
   </AppProvider>
 );
 
---
+++ src/reducer/cartReducer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const cartReducer = (state, action) => {
+    let{id, color, amount, product} = action.payload;
+    
+  return state;
+};
+
+export default cartReducer
